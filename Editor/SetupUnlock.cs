d7e2239c8d082b48f06eb9d1ad15ec87c1410919
Henry Hoffman <henry.hoffman@newfangled.games>
2021-11-15 20:35:09 +0000

    Removed hidden meta files and updated sample and template paths

--- Editor/SetupUnlock.cs
+++ Editor/SetupUnlock.cs
@@ -18,11 +18,11 @@ namespace HenryHoffman.UnlockProtocol
                 Directory.CreateDirectory(webglDir);
             }
 
-            source = "Packages/com.henryhoffman.unlockprotocol/WebGLTemplates/Unlock-custom";
+            source = "Packages/com.henryhoffman.unlockprotocol/WebGLTemplates~/Unlock-custom";
             target = webglDir + "/Unlock-custom";
             AssetDatabase.CopyAsset(source, target);
 
-            source = "Packages/com.henryhoffman.unlockprotocol/WebGLTemplates/Unlock-paywall";
+            source = "Packages/com.henryhoffman.unlockprotocol/WebGLTemplates~/Unlock-paywall";
             target = webglDir + "/Unlock-paywall";
             AssetDatabase.CopyAsset(source, target);
 

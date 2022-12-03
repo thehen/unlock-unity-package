c7eac5e1a12ba214497e69f000086a693d68e382
Henry Hoffman <henry.hoffman@newfangled.games>
2022-12-03 22:53:15 +0000

    added some more warning messaging for unassigned parameters

--- Unlock Unity Package/Editor/Components/UnlockPaywallEditor.cs
+++ Unlock Unity Package/Editor/Components/UnlockPaywallEditor.cs
@@ -14,14 +14,20 @@ namespace HenryHoffman.UnlockProtocol.Paywall
     {
         public override void OnInspectorGUI()
         {
+            UnlockPaywall paywall = (UnlockPaywall)target;
+
             if (!PlayerSettings.WebGL.template.Contains("Unlock-paywall"))
             {
                 EditorGUILayout.HelpBox("Incorrect WebGL Template selected. Go to 'Project Settings > Player Resolution and Presentation' and select Unlock-paywall.", MessageType.Warning);
             }
 
+            if (!paywall.paywallConfig)
+            {
+                EditorGUILayout.HelpBox("Ensure a Paywall Config is assigned below, otherwise the paywall won't work.", MessageType.Warning);
+            }
+
             DrawDefaultInspector();
 
-            UnlockPaywall paywall = (UnlockPaywall)target;
             GameObject paywallGameObject = paywall.gameObject;
 
             if (paywallGameObject.name != "UnlockPaywall")
